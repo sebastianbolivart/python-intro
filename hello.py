c30172437aec028398f254f14c0c43ffa2e19591
Sebastian Bolivar Tapias <sebastian.bolivart@udea.edu.co>
2020-12-19 16:30:06 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 #! /usr/bin/python3
 #print("Hello world");
-user_name = input('Enter your name: ');
-print('Hello', user_name);
+#print('Hello, world')
+#name = input('Enter your name: ')
+#print('Hello', name)
+#name = input('What is the name of your best friend: ')
+#print('Hello Best Friend', name)
+#name = input('es un care gueva')
+print('Bienvenido a Arpanet');
+password1 = input('Ingrese su identificador unico de acceso 1: ');
+password2 = input('Ingrese su identificador unico de acceso 2: ');
+id = (17657456756 + 545752);
+print('El codigo ha sido aceptado', id);
+print('---->Bienvenido a Arpanet<----');
