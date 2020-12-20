a392015741bcbdb2f5fad4dc5ace084a64f0b5f3
Sebastian Bolivar Tapias <sebastian.bolivart@udea.edu.co>
2020-12-20 12:47:58 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,3 +12,36 @@ password2 = input('Ingrese su identificador unico de acceso 2: ');
 id = (17657456756 + 545752);
 print('El codigo ha sido aceptado', id);
 print('---->Bienvenido a Arpanet<----');
+z = """
+	Hello
+		world
+""";
+print(z);
+
+var1 = "Sebastian";
+var2 = int(3);
+var3 = "true";
+
+print("El string ", var1.upper(), "tiene ", len(var1), "caracteres");
+print(type(var2));
+print(type(var3));
+
+my_string = 'Count, the number of     spaces';
+print(my_string[5:]);
+
+print("*" * 10);
+print("Hi" * 10);
+
+title = 'The Good, The Bad, and the Ugly';
+
+print('Source string', title);
+print('Split using  a space');
+print(title.split(' '));
+print('Split using  a coma');
+print(title.split(','));
+
+print("my_string.count(' '):", my_string.count('o'));
+
+welcome_message = 'Hello World';
+print(welcome_message);
+print(welcome_message.replace('Hello', 'Goodbye'));
