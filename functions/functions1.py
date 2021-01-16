6dcda6fdd1278ea34bbea14d141162a65fe6d48e
Sebastian Bolivar Tapias <sebastian.bolivart@udea.edu.co>
2021-01-16 16:08:54 -0500

    functions

--- functions/functions1.py
+++ functions/functions1.py
@@ -1,4 +1,12 @@
 def print_msg(msg):
-    print(msg)	
-print_msg("asdfsafdsfasdfasdfsd")
+    print(msg)
+
 print_msg("Hola mundo, como estas!!")
+
+
+def square(n):
+	return n * n
+
+print (square(2))
+
+print(get_integer_input.__doc__)
